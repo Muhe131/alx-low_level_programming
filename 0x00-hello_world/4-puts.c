51d6d289346f4ce2317e7ac501ec6e5d53e30b50
Muhe131 <mosmn131@gmail.com>
2022-12-08 12:40:04 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 
-/**
- * 
+/*
  * Main - entry point
  *
  * Return: Always 0 (success)
  */
-
-int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
-
+       \t puts("\Programming is like building a multilingual puzzle");
+	return (0);
 }
