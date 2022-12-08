126ab4e354983b00fd31be5036604e92a0cf2063
Muhe131 <mosmn131@gmail.com>
2022-12-08 13:55:26 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ *
+ * main - prints exactly and that piece of art is useful
+ *
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\n" , 59);
+	return(1);
+}
