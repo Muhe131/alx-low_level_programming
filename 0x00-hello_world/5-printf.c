ba4b3af162fe8d90750273ef6e61925b7b4c2bac
Muhe131 <mosmn131@gmail.com>
2022-12-08 23:20:23 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,13 @@
-#include <stdio.h>                                                                                                                                                                                    /**                                                                * Main - entry point                                              *                                                                 * Return: Always 0 (success)                                      */                                                               
+#include <stdio.h>
 
-int main(void)                                                    {                                                                            printf ("with proper grammar, but the outcome is a piece of art");                                                                      return (0);                                        }
+
+/**
+ * Main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)                                                    
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
