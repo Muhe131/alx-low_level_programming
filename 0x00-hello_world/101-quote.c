7a21e5355d6a2d6926ed15ca367169505bed5396
Muhe131 <mosmn131@gmail.com>
2022-12-08 20:52:19 -0800

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  *
  * main - prints exactly and that piece of art is useful
  *
- * return: Always 0 (success)
+ * return: Always 1(error)
  */
 
 int main(void)
 {
-	write (2, "and that piece of art is useful\n" , 59);
+	write(2"that piece of art is useful" - Dora Korpar, 2015-10-19,"\n" , 59);
 	return (1);
 }
