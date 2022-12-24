0e0eb2107ae52ebe97e731b02d71136ab374c66d
Muhe131 <mosmn131@gmail.com>
2022-12-24 01:38:29 -0800

    r

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
-
 /**
- * main - print _putchar
+ * main - Entr point
  *
- * Return: Always 0
+ * Description: A program that prints _putchar followed by a new line
  *
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+	char text[8] = "_putchar";
+	int i = 0;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	_putchar(str[count]);
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
 	_putchar('\n');
-
 	return (0);
 }
