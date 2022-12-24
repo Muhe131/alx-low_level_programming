6e238ce8b56adef34e10e877de08b7207b49e56b
Muhe131 <mosmn131@gmail.com>
2022-12-24 01:48:42 -0800

    s

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints the add of the Fibonacci numbers
- *
- * Return: Always 0.
+ * main - Prints the sum of all multiples of 3 or 5 up to 102
+ * Return: Always (Success)
  */
-
 int main(void)
 {
-	int c;
-	long int n1, n2, fn;
-	n1 = 1;
-	n2 = 2;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 48; c++)
+	int i, z = 0;
+
+	while (i < 1024)
 	{
-		fn = n1 + n2;
-		printf(", %ld", fn);
-		n1 = n2;
-		n2 = fn;																	}
-	printf("\n");
-									return (0);
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+	printf("%d\n", z);
+	return (0);
 }
